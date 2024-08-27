b0c5f9b72ee8225da8a19e9dfc126c6aad9f03fd
Roy Maegus <wolfmagus0@gmail.com>
2024-08-26 23:56:36 -0400

    attempt 2 to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dnd-lang-1/', // Replace with your repository name
   plugins: [react()],
-})
+});
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
